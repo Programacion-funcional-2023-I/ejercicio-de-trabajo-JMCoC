973a0852cb18e532728df48e00d18558eba3c7bf
JMCoC <juan.ramirez.agudelo@correounivalle.edu.co>
2023-08-24 16:30:13 -0500

    Primer commit de prueba - taller FPF

--- app/src/main/scala/introduccion/ListaFactoriales.scala
+++ app/src/main/scala/introduccion/ListaFactoriales.scala
@@ -5,7 +5,7 @@ class ListaFactoriales {
     def objListaFactoriales(n : Int): List[BigInt] = {
         var listaFactoriales : List[BigInt] = List()
         val objFactorial: Factorial = new Factorial()
-        //Complete el código
+
         throw new UnsupportedOperationException("No implementado aún")
     }
 }
